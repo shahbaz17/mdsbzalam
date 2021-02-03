fa94fcdc77ae81d9c9bd7a292ca33eb4d8edf922
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-02-03 08:13:48 +0530

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mohammad Shahbaz Alam
+
+Run `npx mdsbzalam` to get my contact details.
+
+```bash
+npx mdsbzalam
+```
+
+Looking forward to connecting with you.
---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+Mohammad Shahbaz Alam
+
+Website - https://www.mdsbzalam.dev
+Twitter - https://www.twitter.com/mdsbzalam
+LinkedIn - https://www.linkedin.com/in/mdsbzalam
+Youtube - https://www.youtube.com/c/mdsbzalam
+Twitch - https://www.twitch.tv/mdsbzalam
+Instagram - https://www.instagram.com/mdsbzalam
+Facebook - https://www.facebook.com/mdsbzalam
+GitHub - https://www.github.com/shahbaz17
+Dev.to - https://www.dev.to/shahbaz17
+
+Email - hey@mdsbzalam.dev
+`)
