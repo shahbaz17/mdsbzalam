f46ce989ddafb24ac1377e85f5b0b6a5120b5fa0
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2025-11-06 11:42:36 +0400

    Update

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 console.log(`
 Mohammad Shahbaz Alam
 
+ishahbaz.eth - https://app.ens.domains/ishahbaz.eth
+
 Website - https://www.mdsbzalam.dev
-Twitter - https://www.twitter.com/mdsbzalam
+Twitter - https://www.x.com/ishahbaz
 LinkedIn - https://www.linkedin.com/in/mdsbzalam
 Youtube - https://www.youtube.com/c/mdsbzalam
-Twitch - https://www.twitch.tv/mdsbzalam
-Instagram - https://www.instagram.com/mdsbzalam
-Facebook - https://www.facebook.com/mdsbzalam
 GitHub - https://www.github.com/shahbaz17
 Dev.to - https://www.dev.to/shahbaz17
 
